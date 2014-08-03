fda67a038f7d9a137cbe93e7b5b6c36ce1a5c795
Chris Droom <chris.droom@gmail.com>
2014-08-03 15:02:50 +0800

    failing on first pass

--- theme/assets/cart_hil.js
+++ theme/assets/cart_hil.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
   // var getTitles = [];
   var prescriptionQTY = 0;
   var lensesQTY = 0;
+  var totalPrice = 0;
 
   // var test = 0;
 
@@ -22,74 +23,44 @@ $(document).ready(function(){
   }).success();
  };
 
+
+
+
+
  function fUpdateCart(){
 
-  // Reset
-
-  // getTitles.length = 0;
-  // prescriptionQTY = 0;
-  // lensesQTY = 0;
-
-  // Get
-
   jQuery.getJSON('/cart.js', function(data) {
+
+    prescriptionQTY = 0;
 
     $.each(data.items, function(index, element) {
 
       // Get Lenses
 
       if ( element.variant_id == 763367271 ){
-
         lensesQTY = element.quantity;
-
-        // console.log("QTY of", element.handle, element.quantity);
-        // console.log("lensesQTY", lensesQTY);
-
-      } else {
-
-        // console.log("This isn't a High-Index Lens");
-
       }
 
-      // getTitles.push(element.title) // Grab titles
-
       if (element.title.indexOf("With ") >= 0) {
-       
-        // console.log("YO");
 
-        // test++ 
-
-        prescriptionQTY = element.quantity;
-
+        // console.log("element.title", element.title)
         // console.log("element.quantity", element.quantity)
 
-      };  
+        prescriptionQTY += element.quantity;
+        
+        // prescriptionQTY = element.quantity;
+
+
+
+      }; 
 
     });
 
-    console.log("total_price", data.total_price);
+    //console.log("total_price", data.total_price);
+    totalPrice = data.total_price;
 
-    //$("#priceFinal").text(data.total_price);
 
   });
-
-
-
-
-
-  // for ( var i = 0, l = getTitles.length; i < l; i++ ) {
-
-  //   if (getTitles[i].indexOf("With ") >= 0) {
-
-  //     prescriptionQTY ++;
-
-  //   } else {
-
-  //   }
-
-  // }
-
-
 
 
 
@@ -98,9 +69,14 @@ $(document).ready(function(){
 
   console.log("prescriptionQTY", prescriptionQTY);
   console.log("lensesQTY", lensesQTY);
+  console.log("totalPrice", totalPrice);
+
 
 };
 
+
+
+fUpdateCart();
 
 
 $("#add_hil").click(function(){
