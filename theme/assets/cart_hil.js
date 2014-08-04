1cdf8857eaed69d26b61ac1e794e3fdb07a0f78d
Chris Droom <chris.droom@gmail.com>
2014-08-04 15:29:14 +0800

    dynamic lenses, addition and removal feature

--- theme/assets/cart_hil.js
+++ theme/assets/cart_hil.js
@@ -5,93 +5,135 @@ $(document).ready(function(){
   var prescriptionQTY = 0;
   var lensesQTY = 0;
   var totalPrice = 0;
+  var lensePrice = 3500;
 
   // var test = 0;
 
- //  function fCartEmpty(){
- //   jQuery.post('/cart/change.js', {
- //    quantity: 0,
- //    id: 763367271,
- //  }).success();
- // };
 
+  function initCallFunct(fToCall) {
+  
+  }
 
- function fCartAdd(){
-   jQuery.post('/cart/add.js', {
-    quantity: 5,
-    id: 763367271,
-  }).success();
+  function fCartEmpty(fToCall){
+   fToCall = fToCall || function(){}
+
+   jQuery.post('/cart/change.js', {
+    quantity: 0,
+    id: 763367271
+  } ,  function(data) {
+
+   console.log("success empty");
+   //fCartAdd();
+   fToCall();
+  
+ },   "json" );
  };
 
 
+ function fCartAdd(fToCall){
+   fToCall = fToCall || function(){}
+   jQuery.post('/cart/add.js', {
+    quantity: prescriptionQTY,
+    id: 763367271
+  } , function (data) {
+
+      console.log("success add");
+      console.log(fToCall);
+      fToCall();
+
+  }, "json");
+ }
 
 
+ function fPushPrice(){
+  console.log("Batman!");
+  console.log("Change totalPrice to " + totalPrice);
+  $("#priceFinal").text(totalPrice);
+       // $("#priceFinal").currency();
+  }
 
- function fUpdateCart(){
+     function fUpdateCart(fToCall){
+      fToCall = fToCall || function(){}
+      jQuery.getJSON('/cart.js', function(data) {
+        console.log(data);
 
-  jQuery.getJSON('/cart.js', function(data) {
+        prescriptionQTY = 0;
 
-    prescriptionQTY = 0;
-
-    $.each(data.items, function(index, element) {
+        $.each(data.items, function(index, element) {
 
       // Get Lenses
 
       if ( element.variant_id == 763367271 ){
         lensesQTY = element.quantity;
+        lensePrice = element.price;
       }
 
+      // Get Frames with Prescription
+
       if (element.title.indexOf("With ") >= 0) {
-
-        // console.log("element.title", element.title)
-        // console.log("element.quantity", element.quantity)
-
         prescriptionQTY += element.quantity;
-        
-        // prescriptionQTY = element.quantity;
-
-
-
       }; 
 
     });
 
-    //console.log("total_price", data.total_price);
-    totalPrice = data.total_price;
+        totalPrice = data.total_price;
+        fToCall();
+      
+        console.log("totalPrice", totalPrice);
+        console.log("prescriptionQTY", prescriptionQTY);
+        console.log("lensesQTY", lensesQTY);
+        console.log("totalPrice", totalPrice); 
 
 
-  });
+      });
 
 
 
-
-  // Respond
-
-  console.log("prescriptionQTY", prescriptionQTY);
-  console.log("lensesQTY", lensesQTY);
-  console.log("totalPrice", totalPrice);
-
-
+  // Report
 };
 
 
+function fRemoveFromCart() {
+  var update_n_print = function() {
+    fUpdateCart(fPushPrice);
+  }
+  fCartEmpty(update_n_print);
+}
 
-fUpdateCart();
 
 
-$("#add_hil").click(function(){
-  fUpdateCart();
+function fAddToCart () {
+ var update_n_print = function() { fUpdateCart(fPushPrice) }
+ var add_n_print = function() { fCartAdd(update_n_print) }
+ var empty_n_add = function() { fCartEmpty(add_n_print) }
+ fUpdateCart(empty_n_add); 
+}
+
+$("#HIL-ADD").click(function(e){
+  e.preventDefault();
+  fAddToCart();
+
 });
 
-$("#remove_hil").click(function(){
-  fUpdateCart();
+$("#HIL-REMOVE").click(function(e){
+  e.preventDefault();
+  fUpdateCart(fRemoveFromCart);
+  // fCartEmpty();
+
 });
 
-$("#manual_add").click(function(){
-  fCartAdd();
-});
-
-
+// $.ajax({
+//   type: "POST",
+//   url: '/cart/change.js',
+//   data:  { 
+//      quantity: 0,
+//      id: 763367271
+//   },
+//   success: function(data) {
+//     console.log(data);
+//   },
+//   dataType: "json"
+// });
 
 
 });
