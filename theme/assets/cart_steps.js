4c966053bf59202b30f18a7f415d16fecf2f77bf
Chris Droom <chris.droom@gmail.com>
2014-09-04 00:10:54 +0800

    rewrote checkout logic, jQuery validation

--- theme/assets/cart_steps.js
+++ theme/assets/cart_steps.js
@@ -1,103 +1,147 @@
-/* removed */
-
-
 $(document).ready(function(){
-  checkStatus();
 
-  var prescriptionNeeded = false;
-  var prescriptionComplete = false;
+var prescriptionNeeded = false;
+var prescriptionComplete = false;
 
-  var stage2question2_answered = false;
-  var stage2question3_answered = false;
-  var stage2question4_answered = false;
+var stage2question2_answered = false;
+var stage2question3_answered = false;
+var stage2question4_answered = false;
 
+var activeColor = '#a54c3a';
+var inactiveColor = '#333';
 
-
-  // check item quantities
-  var itemQtyPrescription = $('.checkout_buy_prescription_item').length;
-  var itemQtyFrame = $('.checkout_buy_frame_item').length;
-  var itemQtyAcc = $('.checkout_buy_acc_item').length;
-
+var itemQtyPrescription = $('.checkout_buy_prescription_item').length;
+var itemQtyFrame = $('.checkout_buy_frame_item').length;
+var itemQtyAcc = $('.checkout_buy_acc_item').length;
 
 
-  if (itemQtyPrescription > 0){
-    $(".checkout_buy_prescription_title").show();
-    console.log("prescription show")
-    prescriptionNeeded = true;
+// INIT
 
-  } else {
-    $(".checkout_buy_prescription_title").hide();
-    console.log("prescription hide")
-  }
-
-  if (itemQtyFrame > 0){
-    $(".checkout_titles_buy_frames").show();
-  } else {
-    $(".checkout_titles_buy_frames").hide();
-  }
-
-  if (itemQtyAcc > 0){
-    $(".checkout_titles_buy_acc").show();
-  } else {
-    $(".checkout_titles_buy_acc").hide();
-  }
+if (itemQtyPrescription > 0){
+  // There are items in the cart which require the form
+  $(".checkout_buy_prescription_title").show();
+  prescriptionNeeded = true;
+  $('#continue').show();
+  $('#checkout').hide();
 
 
+} else {
+  $(".checkout_buy_prescription_title").hide();
+  $('.checkout_breadcrumb li.step2').remove();
+  $('.checkout_breadcrumb li.step3').html($('.checkout_breadcrumb li.step3').html().replace('3.', '2.'));
+
+  prescriptionNeeded = false;
+  $('#continue').hide();
+  $('#checkout').show();
+}
 
 
-  function enableCheckout(){
-    // $('#checkout').css('background', 'green');
+if (itemQtyFrame > 0){
+  $(".checkout_titles_buy_frames").show();
+} else {
+  $(".checkout_titles_buy_frames").hide();
+}
 
-   // $('#checkout').addClass('enabled');
-
-
-
-
-    $('#checkout').attr('disabled', false);
-    $('#checkout').val('Proceed to Review & Payment →');
-  };
-
-  function disableCheckout(){
-    // $('#checkout').css('background', '#333');
-    $('#checkout').attr('disabled', true);
-    $('#checkout').val("Please fill in all fields to proceed");
-  };
+if (itemQtyAcc > 0){
+  $(".checkout_titles_buy_acc").show();
+} else {
+  $(".checkout_titles_buy_acc").hide();
+}
 
 
+checkStatus();
+stage2question2_hideSecondary();
+stage2question4_hideSecondary();
 
 
-  function checkStatus(){
+$('.checkout_stage2').hide();
+$('.checkout_breadcrumb .step1 a').css('color', activeColor);
 
-    console.log("stage2question2_answered", stage2question2_answered);
-    console.log("stage2question3_answered", stage2question3_answered);
-    console.log("stage2question4_answered", stage2question4_answered);
-
-    // console.log("localStorage['stage2question2_answered']", localStorage['stage2question2_answered']);
+// Nav
 
 
+$('#continue').click(function(){
+  $('.checkout_stage1').hide();
+  $('.checkout_stage2').fadeIn(200);
+  $('#continue').hide();
+  $('#checkout').fadeIn(200);
+  $('.checkout_breadcrumb .step1 a').css('color', inactiveColor);
+  $('.checkout_breadcrumb .step2 a').css('color', activeColor);
+});
+
+
+$('.checkout_breadcrumb .step1 a').click(function(){
+  $('.checkout_stage1').fadeIn(200);
+  $('.checkout_stage2').hide();
+  $('#continue').fadeIn(200);
+  $('#checkout').hide();
+  $('.checkout_breadcrumb .step1 a').css('color', activeColor);
+  $('.checkout_breadcrumb .step2 a').css('color', inactiveColor);
+});
+
+$('.checkout_breadcrumb .step2 a').click(function(){
+  $('.checkout_stage1').hide();
+  $('.checkout_stage2').fadeIn(200);
+  $('#continue').hide();
+  $('#checkout').fadeIn(200);
+  $('.checkout_breadcrumb .step1 a').css('color', inactiveColor);
+  $('.checkout_breadcrumb .step2 a').css('color', activeColor);
+});
+
+
+// Form Status
+
+function enableCheckout(){
+ $('#checkout').attr('disabled', false);
+ $('#checkout').val('Proceed to Review & Payment →');
+};
+
+function disableCheckout(){
+  $('#checkout').attr('disabled', true);
+  $('#checkout').val("Please fill in all fields to proceed");
+};
+
+function checkStatus(){
+  // console.log("stage2question2_answered", stage2question2_answered);
+  // console.log("stage2question3_answered", stage2question3_answered);
+  // console.log("stage2question4_answered", stage2question4_answered);
+  console.log("prescriptionNeeded", prescriptionNeeded);
+
+  if (prescriptionNeeded) {
 
     if (stage2question2_answered && stage2question3_answered && stage2question4_answered){
-
       enableCheckout();
-      console.log("enable checkout");
-
+      console.log("checkout: enabled");
     } else {
-
-     disableCheckout();
-     console.log("disable checkout");
-
-   }
+      disableCheckout();
+      console.log("checkout: disabled");
+    }
 
 
-
-
- };
-
+  } else {
+    enableCheckout();
+    console.log("checkout: enabled");
+  }
 
 
 
+};
 
- function activeQuestion(whichQuestion, thisPassed){
+
+function stage2question2_hideSecondary(){
+  $('.prescription-doctor').hide();
+  $('.prescription-upload').hide();
+  $('.prescription-address').hide();
+}
+
+function stage2question4_hideSecondary(){
+  $('.prescription-pd').hide();
+}
+
+
+// Buttons
+
+function activeQuestion(whichQuestion, thisPassed){
   $(whichQuestion+' label').css('border', '#e7e7e7 1px solid');
   $(whichQuestion+' .radioWrap .radioLabel').css('-webkit-box-shadow', '1px 1px 1px #e7e7e7');
   $(whichQuestion+' .radioWrap .radioLabel').css('-moz-box-shadow', '1px 1px 1px #e7e7e7');
@@ -108,28 +152,57 @@ $(document).ready(function(){
   $(thisPassed).css('box-shadow', 'inset 2px 2px 2px #e5e5e5');
 }
 
-
-
-
 $('.stage2question2 .radioWrap .radioLabel').click(function() {
   stage2question2_answered = true;
-  // localStorage['stage2question2_answered'] = stage2question2_answered;
   activeQuestion('.stage2question2', this);
   checkStatus();
 });
 
 $('.stage2question3 .radioWrap .radioLabel').click(function() {
   stage2question3_answered = true;
-  // localStorage['stage2question3_answered'] = stage2question3_answered;
   activeQuestion('.stage2question3', this);
   checkStatus();
 });
 
 $('.stage2question4 .radioWrap .radioLabel').click(function() {
   stage2question4_answered = true;
-  // localStorage['stage2question4_answered'] = stage2question4_answered;
   activeQuestion('.stage2question4', this);
   checkStatus();
+});
+
+
+
+// SHOW AND HIDE
+
+// Section 2
+
+$('.upload-a-picture').click(function() {
+  stage2question2_hideSecondary();
+  $('.prescription-upload').show();
+  console.log("upload-a-picture");
+});
+
+$('.call-my-doctor').click(function() {
+  stage2question2_hideSecondary();
+  $('.prescription-doctor').show();
+  console.log("call-my-doctor");
+});
+
+$('.send-later').click(function() {
+  console.log("send-later");
+  stage2question2_hideSecondary();
+  $('.prescription-address').show();
+});
+
+
+// Section 3
+
+$('.measure').click(function() {
+  $('.prescription-pd').css('display', 'block');
+});
+
+$('.dont-measure').click(function() {
+  $('.prescription-pd').css('display', 'none');
 });
 
 
